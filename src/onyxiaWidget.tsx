0358da9f18e912cbe90d031bdb1cc1ed37ae5e7e
Olivier Giorgis <ogiorgis@logilab.fr>
2024-04-29 14:39:35 +0200

    add  fields to form

--- src/onyxiaWidget.tsx
+++ src/onyxiaWidget.tsx
@@ -6,7 +6,17 @@ export const OnyxiaComponent = (): JSX.Element => {
   return (
     <div>
       <h1>Onyxia Composer</h1>
-
+      <h2>Service</h2>
+      <h3>Name</h3>
+      <input type="text" required size={30} />
+      <h3>Description</h3>
+      <input type="text" required size={30} />
+      <h3>Icon Url</h3>
+      <input type="text" required size={30} />
+      <h4>Type</h4>
+      <ul>
+        <li> voila </li>
+      </ul>
       <h2>Docker image</h2>
       <input type="text" required size={30} />
       <section>
