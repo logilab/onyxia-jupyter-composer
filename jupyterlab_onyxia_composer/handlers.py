d82d78bd3d8e7be72604dd5cbb83a29dc741fa03
Olivier Giorgis <ogiorgis@logilab.fr>
2024-04-30 09:59:41 +0200

    charts service config generation

--- jupyterlab_onyxia_composer/handlers.py
+++ jupyterlab_onyxia_composer/handlers.py
@@ -1,9 +1,13 @@
 import json
+from pathlib import Path
+import os
+import shutil
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+DEFAULT_VOILA_ICON_URL = "https://raw.githubusercontent.com/voila-dashboards/voila/main/docs/voila-logo.svg"
 
 class RouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
@@ -19,6 +23,7 @@ class RouteHandler(APIHandler):
     def post(self):
         # input_data is a dictionary with a key "name"
         input_data = self.get_json_body()
+        create_service(input_data)
         data = {"message": "Service {} is created".format(input_data["name"])}
         self.finish(json.dumps(data))
 
@@ -31,3 +36,28 @@ def setup_handlers(web_app):
     route_pattern = url_path_join(base_url, "jupyterlab-onyxia-composer", "create")
     handlers = [(route_pattern, RouteHandler)]
     web_app.add_handlers(host_pattern, handlers)
+
+
+def create_service(data):
+    github_repo_dir = Path.home() / "helm-charts-logilab-services"
+    voila_template_dir = github_repo_dir / "charts-template" / "voila"
+    service_name = data["name"].strip().replace(' ', '_')
+    new_service_dir = github_repo_dir / "charts" / service_name
+    try:
+        os.mkdir(new_service_dir)
+    except:
+        raise Exception("This service already exist")
+    for finput in os.listdir(voila_template_dir):
+        if os.path.isdir(voila_template_dir / finput):
+            shutil.copytree(voila_template_dir / finput, new_service_dir / finput)
+        else:
+            with open(voila_template_dir / finput, 'r') as inf:
+                with open(new_service_dir / finput, 'w') as outf:
+                    for line in inf:
+                        outf.write(
+                            line
+                            .replace("${NAME}", data["name"])
+                            .replace("${DESCRIPTION}", data.get('desc', ''))
+                            .replace("${IMAGE}",  data['dockerImg'])
+                            .replace("${ICONURL}", data.get('iconURL', DEFAULT_VOILA_ICON_URL))
+                        )
